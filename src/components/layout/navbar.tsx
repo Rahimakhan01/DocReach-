5a973ec12254880a42c728758893d62461a6d752
Rahimakhan01 <khanrahima1311@gmail.com>
2025-02-07 03:06:02 +0530

    navbar updated

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar() {
   const { user, isAuthenticated, logout } = useAuth();
 
   return (
-    <nav className="fixed top-0 w-full bg-gradient-to-r from-[#091540] via-[#7692ff] to-[#3d518c] backdrop-blur-sm border-b z-50">
+    <nav className="fixed top-0 w-full bg-gradient-to-r from-[#496ecb] via-[#a4dbf6] to-[#cdfbfd] backdrop-blur-sm border-b z-50">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link to="/" className="flex items-center space-x-2">
           <Stethoscope className="w-8 h-8 text-[#abd2fa]" />
@@ -18,7 +18,7 @@ export function Navbar() {
           {isAuthenticated ? (
             <>
               <Link to={user?.role === 'doctor' ? '/doctor-dashboard' : '/dashboard'}>
-                <Button variant="ghost" className="flex items-center space-x-2 text-[#35f7e7] hover:text-[#3d518c]">
+                <Button variant="ghost" className="flex items-center space-x-2 text-white hover:text-[#5863F8]">
                   <User className="w-4 h-4" />
                   <span>Dashboard</span>
                 </Button>
@@ -26,7 +26,7 @@ export function Navbar() {
               <Button
                 variant="ghost"
                 onClick={logout}
-                className="flex items-center space-x-2 text-[#35f7e7] hover:text-[#3d518c]"
+                className="flex items-center space-x-2 text-white hover:text-[#5863F8]"
               >
                 Logout
               </Button>
@@ -34,12 +34,12 @@ export function Navbar() {
           ) : (
             <>
               <Link to="/login">
-                <Button className="bg-[rgb(79,42,246)] text-white hover:bg-[#16bac5]">
+                <Button className="bg-[#16BAC5] text-white hover:bg-[#5863F8]">
                   Login
                 </Button>
               </Link>
               <Link to="/signup">
-              <Button className="bg-[rgb(79,42,246)] text-white hover:bg-[#16bac5]">
+              <Button className="bg-[#16BAC5]  text-white hover:bg-[#5863F8]">
                   Sign Up
                 </Button>
               </Link>
